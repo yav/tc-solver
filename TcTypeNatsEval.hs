f42008b4276c766da78da481b768ba39d8d00713
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2011-12-10 14:13:14 -0800

    Document functions for concrete evaluation.

--- TcTypeNatsEval.hs
+++ TcTypeNatsEval.hs
@@ -1,11 +1,15 @@
-{-------------------------------------------------------------------------------
-Functions on natural numbers.
--------------------------------------------------------------------------------}
+{-| This module defines the inverse functions used for simplifying
+propositions using concrete natural numbers.
+-}
+
 module TcTypeNatsEval where
 
+-- | Subtract two natural numbers.
 minus :: Integer -> Integer -> Maybe Integer
 minus x y = if x >= y then Just (x - y) else Nothing
 
+-- | Compute the exact root of two natural numbers.
+-- The second argument specifies which root we are computing.
 descreteRoot :: Integer -> Integer -> Maybe Integer
 descreteRoot x0 root = search 0 x0
   where
@@ -17,6 +21,8 @@ descreteRoot x0 root = search 0 x0
                         GT | x /= to    -> search from x
                         _               -> Nothing
 
+-- | Compute the exact logarithm of two natural numbers.
+-- The logarithm base is the second argument.
 descreteLog :: Integer -> Integer -> Maybe Integer
 descreteLog 0  _  = Just 0
 descreteLog x0 base | base == x0  = Just 1
@@ -24,6 +30,7 @@ descreteLog x0 base = case divMod x0 base of
                          (x,0) -> fmap (1+) (descreteLog x base)
                          _     -> Nothing
 
+-- | Divide two natural numbers.
 divide :: Integer -> Integer -> Maybe Integer
 divide _ 0  = Nothing
 divide x y  = case divMod x y of
